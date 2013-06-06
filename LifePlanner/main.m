abd07bdc53f4b8329884af77a5fa0fec0f83cd89
Theodore Calmes <theo@thoughtbot.com>
2013-06-05 19:25:39 -0700

    Initial Commit

---
+++ LifePlanner/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LifePlanner
+//
+//  Created by Theodore Calmes on 6/5/13.
+//  Copyright (c) 2013 theo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TCAppDelegate class]));
+    }
+}
---
+++ LifePlannerTests/LifePlannerTests.h
@@ -0,0 +1,13 @@
+//
+//  LifePlannerTests.h
+//  LifePlannerTests
+//
+//  Created by Theodore Calmes on 6/5/13.
+//  Copyright (c) 2013 theo. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface LifePlannerTests : SenTestCase
+
+@end
